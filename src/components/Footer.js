1dae7d93fde197c66a5fa52b922e90bfb95eba8e
TanmayeeVeerabhadra <tanmayeev066@gmail.com>
2022-10-09 19:34:28 +0530

    COMMITED

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../myStyles.css";
+const Footer = () => {
+  return (
+    <div>
+      <div className="footer">
+        <div className="footer-content">
+            <h3>My FED Blog</h3>
+            <p>20BQ1A05O9</p>
+            <p>copyright &copy;2021 <a href="#"> My FED Blog</a>  </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Header = () => {
+  return (
+    <div>
+        <div className="header">
+          <div className='headerheading'>         
+           Implement Upload and Download Options on a given file
+          </div>
+            <Navbar />
+        </div>
+    </div>
+  )
+}
+
+export default Header
